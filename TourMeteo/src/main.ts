fc0791bfbbd07836000dbf8a656ee11acf3d6cfc
Hugo <hugo.lembrez@gmail.com>
2026-02-25 12:43:09 +0100

    🔧 Vercel analytics

--- TourMeteo/src/main.ts
+++ TourMeteo/src/main.ts
@@ -3,8 +3,13 @@ import { appConfig } from './app/app.config';
 import { RootComponent } from './app/root.component';
 import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 registerLocaleData(localeFr);
+
+inject();
+injectSpeedInsights();
 
 bootstrapApplication(RootComponent, appConfig)
   .catch((err) => console.error(err));
